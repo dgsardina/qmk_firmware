365373e332421571c21f1403201c6ee8a24940b4
Daniel Garcia Sardina <dgs@navarino.gr>
2022-03-01 18:01:50 +0200

    Moving space to left hand

--- keyboards/splitkb/kyria/keymaps/dgsardina/keymap.c
+++ keyboards/splitkb/kyria/keymaps/dgsardina/keymap.c
@@ -45,6 +45,7 @@ enum layers {
 
 #define RALT_ENT MT(MOD_RALT, KC_ENT)
 #define SYM_BSPC LT(     SYM, KC_BSPC)
+#define NAV_ENT LT(     SYM, KC_BSPC)
 
 // Overrides
 const key_override_t delete_key_override =   ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
@@ -77,15 +78,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * |Shift/<>|   Z  |   X  |   C  |   V  |   B  | [ {  |CapsLk|  |F-keys|  ] } |   N  |   M  |  , ; |  . : |  - _ |Shift/+*|
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |Adjust| LGUI | LAlt | Enter| Nav  |  | Sym/ | Space| AltGr| RGUI | Menu |
- *                        |      |      |      |      |      |  | Bspc |      |      |      |      |
+ *                        |Adjust| LGUI | LAlt/| Space| Nav  |  | Sym  | Back | AltGr| RGUI | Menu |
+ *                        |      |      | Enter|      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P ,   ES_GRV,
      CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,ES_NTIL, CTL_ACUT,
      LSFT_LT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , ES_LBRC,KC_CAPS,     FKEYS  , ES_RBRC, KC_N,   KC_M ,ES_COMM, ES_DOT ,ES_MINS, RSFT_PLS,
-                                ADJUST , KC_LGUI, KC_LALT, KC_ENT, NAV   ,    SYM_BSPC , KC_SPC,KC_RALT,KC_RGUI,KC_APP
+                                 ADJUST, KC_LGUI, ALT_ENT, KC_SPC , NAV   ,     SYM    , KC_BSPC ,KC_RALT, KC_RGUI, KC_APP
     ),
 /*
  * Base Layer: Dvorak
