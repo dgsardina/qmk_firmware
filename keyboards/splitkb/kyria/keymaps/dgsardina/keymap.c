0b383536344c9891c62e92e000669e56e3026a38
Daniel Garcia Sardina <dgs@navarino.gr>
2022-03-08 11:33:23 +0200

    Changing from temporary switch to layers to toggling them

--- keyboards/splitkb/kyria/keymaps/dgsardina/keymap.c
+++ keyboards/splitkb/kyria/keymaps/dgsardina/keymap.c
@@ -46,6 +46,7 @@ enum layers {
 #define RALT_ENT MT(MOD_RALT, KC_ENT)
 #define SYM_BSPC LT(     SYM, KC_BSPC)
 #define NAV_ENT LT(     SYM, KC_BSPC)
+#define OSM_LSFT OSM(MOD_LSFT)
 
 // Overrides
 const key_override_t delete_key_override =   ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
@@ -86,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P ,   ES_GRV,
      CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,ES_NTIL, CTL_ACUT,
      LSFT_LT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , ES_LBRC,KC_CAPS,     FKEYS  , ES_RBRC, KC_N,   KC_M ,ES_COMM, ES_DOT ,ES_MINS, RSFT_PLS,
-                                 ADJUST, KC_LGUI, ALT_ENT, KC_SPC , NAV   ,     SYM    , KC_BSPC ,KC_RALT, KC_RGUI, KC_APP
+                                 ADJUST, KC_LGUI, ALT_ENT, KC_SPC ,OSM_LSFT   , TO(_SYM), KC_BSPC ,KC_RALT, KC_RGUI, KC_APP
     ),
 /*
  * Base Layer: Dvorak
@@ -148,7 +149,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       ES_MORD,   ES_1 ,   ES_2 ,   ES_3 ,   ES_4 ,   ES_5 ,                                       ES_6 ,   ES_7 ,  ES_8 ,    ES_9 ,   ES_0 , ES_QUOT,
       ES_FORD, ES_EXLM, ES_DQUO, ES_BULT,  ES_DLR, ES_PERC,                                     ES_AMPR, ES_SLSH, ES_LPRN, ES_RPRN,  ES_EQL, ES_QUES,
 ES_BSLS , ES_PIPE,  ES_AT , ES_HASH, ES_TILD, ES_EURO, _______, _______, _______, _______, ES_IQUE, ES_IEXL, ES_ASTR, _______, ES_CCED, S(ES_CCED),
-                                 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+                                 _______, _______, _______, TO(0), _______, TO(_NAV), _______, _______, _______, _______
     ),
 
 /*
@@ -169,7 +170,7 @@ ES_BSLS , ES_PIPE,  ES_AT , ES_HASH, ES_TILD, ES_EURO, _______, _______, _______
       _______, _______, _______, _______, _______, _______,                                     _______, KC_HOME, _______,   KC_END,  KC_VOLU, KC_DEL,
       _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______,                                     KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_VOLD, KC_INS,
       _______, _______, _______, _______, _______, _______, _______, KC_SLCK, _______, _______,KC_PAUSE, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_PSCR,
-                                 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+                                 _______, _______, _______, TO(0), _______, TO(_NAV), _______, _______, _______, _______
     ),
 
 /*
