8b3c5f4fe9802e2309c3efab21670dc47108f3fd
Daniel Garcia Sardina <dgs@navarino.gr>
2022-03-21 16:17:30 +0200

    Activating RGB underglow leds

--- keyboards/splitkb/kyria/keymaps/dgsardina/rules.mk
+++ keyboards/splitkb/kyria/keymaps/dgsardina/rules.mk
@@ -1,7 +1,9 @@
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306   # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
-RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 KEY_OVERRIDE_ENABLE = yes
 #POINTING_DEVICE_ENABLE = yes
 #POINTING_DEVICE_DRIVER = pimoroni_trackball
+# Disable either to fit RGBLIGHT
+MAGIC_ENABLE = no       # MAGIC_ actions (BOOTMAGIC without the boot)
